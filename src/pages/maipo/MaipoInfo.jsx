bdb78102316538e6700c7b6ffe246b711f24e664
hyein <khi_9866@naver.com>
2022-07-10 16:48:53 +0900

    feature:maipo 페이지 구성

---
+++ src/pages/maipo/MaipoInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MaipoInfo = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MaipoInfo;
---
+++ src/pages/maipo/MaipoPro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MaipoPro = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MaipoPro;
