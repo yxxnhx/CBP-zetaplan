3973988d8edf3f421c6f0391e4f415eedc2e6b67
sejineeee <sejinee0918@gmail.com>
2022-07-10 11:43:16 +0900

    feature: global style 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
