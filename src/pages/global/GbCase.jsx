58d94f64fa8d175bab22d78e186a58d494d92fb2
kimmjiwon <k980912@naver.com>
2022-07-10 17:05:44 +0900

    feature:global 페이지 구성

---
+++ src/pages/global/GbCase.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GbCase = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default GbCase;
---
+++ src/pages/global/GbStategy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GbStategy = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default GbStategy;
