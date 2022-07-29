cad4b9aa31713880fc53f21ddfa0544b7f801939
kimmjiwon <k980912@naver.com>
2022-07-29 22:32:44 +0900

    our business 동영상 재생 수정

--- src/pages/main/OurBusiness.jsx
+++ src/pages/main/OurBusiness.jsx
@@ -13,7 +13,7 @@ const OurBusiness = () => {
         <div className="obInner">
           <div className="obList professional">
             <div className="videoCon">
-              <video src={professional} loop muted autoPlay playsinline></video>
+              <video src={professional} autoPlay loop muted playsInline></video>
               <div className="obVideoDes">
                 <p className="obVideoDesT">Professional</p>
                 <p className="obVideoDesC">각 분야의 전문가들이 돕겠습니다.</p>
@@ -22,7 +22,7 @@ const OurBusiness = () => {
           </div>
           <div className="obList global">
             <div className="videoCon">
-              <video src={global} loop muted autoPlay playsinline></video>
+              <video src={global} autoPlay loop muted playsInline></video>
               <div className="obVideoDes">
                 <p className="obVideoDesT">Global</p>
                 <p className="obVideoDesC">다양한 해외지사를 보유하고 있습니다.</p>
@@ -31,7 +31,7 @@ const OurBusiness = () => {
           </div>
           <div className="obList trust">
             <div className="videoCon">
-              <video src={trust} loop muted autoPlay playsinline></video>
+              <video src={trust} autoPlay loop muted playsInline></video>
               <div className="obVideoDes">
                 <p className="obVideoDesT">Customize</p>
                 <p className="obVideoDesC">기업에 맞춘 컨설팅을 지원합니다.</p>
@@ -40,7 +40,7 @@ const OurBusiness = () => {
           </div>
           <div className="obList customized">
             <div className="videoCon">
-              <video src={customized} loop muted autoPlay playsinline></video>
+              <video src={customized} autoPlay loop muted playsInline></video>
               <div className="obVideoDes">
                 <p className="obVideoDesT">Trust</p>
                 <p className="obVideoDesC">믿고 맡길 수 있는 서비스를 제공합니다.</p>
